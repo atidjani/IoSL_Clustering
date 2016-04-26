27a507c83aea499d765e9c9d716c89ee9b353f2d
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-04-26 18:51:33 +0200

    Add script to visualize output

---
+++ Scripts/visualizer.py
@@ -0,0 +1,37 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import sys
+import numpy
+
+def plot(resMatrix):
+    fig = plt.figure(figsize=(9,9))
+    numClusters = len(set([i[2] for i in resMatrix]))
+    colors = numpy.random.rand(numClusters, 3)
+    for i in range(numClusters):
+        plt.scatter([x[0] for x in resMatrix if x[2] == i],
+                    [x[1] for x in resMatrix if x[2] == i],
+                    c = colors[i],
+                    edgecolors='none')
+
+    plt.savefig('output.pdf', format='pdf')
+
+
+def main(inputFile):
+    # Open Input file
+    f = open(inputFile, 'r')
+
+    # Read data into a matrix
+    resMatrix = []
+    for line in f:
+        input = line.split(' ', 4);
+        resMatrix.append([int(input[0]), int(input[1]), int(input[2])])
+
+    plot(resMatrix)
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Usage: python visualizer [file]")
+        exit(0)
+    else:
+        main(sys.argv[1])
