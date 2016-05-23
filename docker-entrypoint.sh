a52ae2ed82dcabb89cb8d83012c0e1e3fd81c735
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-05-23 22:13:00 +0200

    fix path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Run the Django development server
-python /srv/visualizer/Visualizer/manage.py runserver 0.0.0.0:8000
+echo $(pwd)
+python Visualizer/manage.py runserver 0.0.0.0:8000
