74d2c676c282f12dafe2643f725fb75aa2bb00e4
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-05-25 23:24:19 +0200

    several bug fixes

--- Visualizer/App/models.py
+++ Visualizer/App/models.py
@@ -1,3 +1,36 @@
+import io
+import hashlib
+import time
+
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
+
+class Dataset(models.Model):
+    creationTime = models.DateField(auto_now_add=True)
+    data = models.TextField();
+
+    def clean(self) :
+        buffer = io.StringIO(self.data)
+        for line in buffer.readlines():
+            features = line.split(',')
+
+                # Check if 2 elements where extracted.
+            if len(features) != 2 :
+                raise ValidationError(_('Format not compatible'))
+
+                # Check if all elements are float
+            for f in features[:-1]:
+                try :
+                    float(f)
+                except ValueError:
+                   raise ValidationError(_('Format not compatible'))
+        return True
+
+    def writeFile(self) :
+        filePath = "/tmp/" + hashlib.md5(str(time.time()).encode('utf-8')).hexdigest()
+        with open(filePath, "w") as f:
+            f.write(self.data);
+        return filePath
--- Visualizer/App/views.py
+++ Visualizer/App/views.py
@@ -1,10 +1,11 @@
-import json
+import os
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.exceptions import ValidationError
 
 from .forms import UploadDatasetForm
-from .dataset import Dataset
+from .models import Dataset
 from .runners import StscRunner
 
 
@@ -13,14 +14,15 @@ def UploadDatasetView(request):
     if request.method == 'POST':
         form = UploadDatasetForm(request.POST, request.FILES)
         if form.is_valid() :
-            ds = Dataset(request.FILES['file'], str(request.session.session_key)[:30])
-            if ds.checkDataset() :
-                # Set the session dataset
-                request.session['ds'] = json.dumps(ds.__dict__)
-                return HttpResponseRedirect('/result')
-            else :
-                ds = None
+            ds = Dataset(data=request.FILES['file'].read())
+            try :
+                ds.full_clean()
+            except ValidationError:
                 return HttpResponse("Error")
+
+            ds.save()
+            request.session['ds'] = ds.id
+            return HttpResponseRedirect('/result')
     else :
         # Trick to avoid None session_key at the first request
         request.session['one'] = "One entry to rule them all and in the session bind them"
@@ -33,8 +35,11 @@ def ResultView(request) :
     if request.session.get('ds', None) == None :
         return HttpResponseRedirect('/')
 
-    # Execution of STSC
-    ds = type('Dataset', (object,), json.loads(request.session['ds']))
-    stsc = StscRunner(ds.filePath, 10, 6)
+    # Get element
+    dsId = request.session.get('ds')
+    ds = Dataset.objects.get(id=dsId)
+    filePath = ds.writeFile() # Write dataset on disk
+    stsc = StscRunner(filePath, 10, 6) # Execution of STSC
     output = stsc.run()
+    os.remove(filePath) # Delete File
     return render(request, 'ResultTemplate.html', output)
