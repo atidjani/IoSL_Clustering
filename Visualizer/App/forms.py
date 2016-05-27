2077c69586f0c1cd5af5e82e66cdf56aec7b66de
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-05-27 16:09:57 +0200

    add style to template

--- Visualizer/App/forms.py
+++ Visualizer/App/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class UploadDatasetForm(forms.Form):
-    file = forms.FileField()
-    algorithm = forms.ChoiceField([('STSC', 'STSC'), ('OPTICS', 'OPTICS')], widget=forms.RadioSelect())
+    file = forms.FileField(label='')
+    algorithm = forms.ChoiceField([('STSC', 'STSC'), ('OPTICS', 'OPTICS')], widget=forms.RadioSelect(), label='')
 
 class ParametersSTSC(forms.Form):
-    numClusters = forms.DecimalField(required=True,min_value=1)
+    numClusters = forms.DecimalField(required=True,min_value=1,label='# Clusters')
     k = forms.DecimalField(required=True,min_value=0)
