0cee5fc7dfe0952f8751d1bb65fd3d1cb96c411d
Ahmed Tidjani <a.tidjani@campus.tu-berlin.de>
2016-05-31 20:19:28 +0200

    progress gradient clustering

--- OPTICS/R/Gradiant Clustering/gradient_clustering.cpp
+++ OPTICS/R/Gradiant Clustering/gradient_clustering.cpp
@@ -1,6 +1,6 @@
 #include <Rcpp.h>
-#include "ANN/ANN.h"
-#include "R_regionQuery.h"
+//#include "ANN.h"
+//#include "R_regionQuery.h"
 #include <math.h>
 #include <stack>
 #include <set>
@@ -18,23 +18,27 @@ using namespace Rcpp;
 //
 
 int w = 1;
-
 float II(int y,NumericVector reachdist){
   //taking w = 1
-  float reachdist1 = reachdist[y] - reachdist[y-1];
-  float reachdist2 = reachdist[y] - reachdist[y+1];
-  return (-w + reachdist1 * reachdist2)
-    /(sqrt(pow(w,2) + pow(reachdist1,2)) * sqrt(pow(w,2) + pow(reachdist2,2)));
+  float reachdist_yx = reachdist[y] - reachdist[y-1];
+  float reachdist_yz = reachdist[y] - reachdist[y+1];
+  std::cout << (-pow(w,2) + reachdist_yx * reachdist_yz)
+    /(sqrt(pow(w,2) + pow(reachdist_yx,2)) * sqrt(pow(w,2) + pow(-reachdist_yz,2)));
+  return (-pow(w,2) + reachdist_yx * reachdist_yz)
+    /(sqrt(pow(w,2) + pow(reachdist_yx,2)) * sqrt(pow(w,2) + pow(-reachdist_yz,2)));
 }
 
 float GD(int y,NumericVector reachdist){
+  std::cout << "GD";
+  std::cout << w * (reachdist[y+1] - reachdist[y]) + w * (reachdist[y-1] - reachdist[y]);
   return w * (reachdist[y+1] - reachdist[y]) + w * (reachdist[y-1] - reachdist[y]);
 }
 
-std::set<int> BuildCluster(IntegerVector co, int startPnt, int endPnt){
+std::set<int> BuildCluster(IntegerVector co, int startPnt, int endPnt, NumericVector clusterId, int id){
   std::set<int> cluster;
   for(int i = startPnt; i <= endPnt; i++){
     cluster.insert(co[i]);
+    if(clusterId[i] == -1)clusterId[i]=id;
   }
   return cluster;
 }
@@ -45,10 +49,12 @@ void helloWorld(){
 }
 
 // [[Rcpp::export]]
-List gradient_clustering(IntegerVector co, NumericVector reachdist, int minPts, float t) {
+NumericVector gradient_clustering(IntegerVector co, NumericVector reachdist, int minPts, float t) {
+  NumericVector clusterId(co.size(),-1);
   std::stack<int> startPts;
   std::set<std::set<int> > setOfClusters;
   std::set<int> currentCluster;
+  int id  = 0;
   int i =0;
   int o = co[i];//first point of the ordering 
   startPts.push(i);// pushing the index of the point instead of the point itself
@@ -65,33 +71,37 @@ List gradient_clustering(IntegerVector co, NumericVector reachdist, int minPts, 
           }
 
           currentCluster.clear();
+          
           if(reachdist[startPts.top()] <= reachdist[i]){
             startPts.pop();
           }
 
           while(reachdist[startPts.top()] < reachdist[i]){
-            setOfClusters.insert(BuildCluster(co,startPts.top(),i));
+            setOfClusters.insert(BuildCluster(co,startPts.top(),i,clusterId, id));
+            id++;
             startPts.pop();
           }
 
           //setOfClusters.insert("set of objects from startPts.top() to last end point");
-          setOfClusters.insert(BuildCluster(co,startPts.top(),i));
+          setOfClusters.insert(BuildCluster(co,startPts.top(),i,clusterId, id));
+          id++;
 
-          if(reachdist[co[i+1]] < reachdist[i]){
+          if(reachdist[i+1] < reachdist[i]){
             startPts.push(i);
           }
 
         }else{
-          if(reachdist[co[i+1]] > reachdist[i]){
+          if(reachdist[i+1] > reachdist[i]){
             //currCluster := set of objects from startPts.top() to o;
-            currentCluster = BuildCluster(co,startPts.top(),i);
+            currentCluster = BuildCluster(co,startPts.top(),i,clusterId, id);
+            id++;
           }
         }
       }
     }else{
       while(!startPts.empty()){
         //currCluster := set of objects from startPts.top() to o;
-        currentCluster = BuildCluster(co,startPts.top(),i);
+        currentCluster = BuildCluster(co,startPts.top(),i,clusterId, id);
         if((reachdist[startPts.top()] > reachdist[i]) && (currentCluster.size() >= minPts)){
           //setOfClusters.add(currCluster);
           setOfClusters.insert(currentCluster);
@@ -99,7 +109,6 @@ List gradient_clustering(IntegerVector co, NumericVector reachdist, int minPts, 
         startPts.pop();
       }
     }
-
   }
 
   //return list of clusters
@@ -112,7 +121,8 @@ List gradient_clustering(IntegerVector co, NumericVector reachdist, int minPts, 
     listOfClusters.insert(listOfClusters.end(),cluster);
   }
 
-  return listOfClusters;
+  //return listOfClusters;
+  return clusterId;
 }
 
 
