76a6e3d4d720a268eb785e31aae44fce2748477a
zahinazher <zahinazher@gmail.com>
2016-06-02 23:16:57 +0200

    cronjob fix

--- Visualizer/App/cron.py
+++ Visualizer/App/cron.py
@@ -1,5 +1,18 @@
 import time
-from models import Dataset
+from __future__ import absolute_import
+import os,django,sys
+from django.conf import settings
+path = os.getcwd()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Visualizer.settings")
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+import django
+django.setup()
+
+from .models import Dataset
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'%s' %path)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'%s/Visualizer' %path)))
 
 # Cronjob, delete entries in the database older than 6 hours
 def deleteOldEntries() :
