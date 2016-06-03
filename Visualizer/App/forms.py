e44e63c41b4352a440a2f6a8844ffedbeb6491df
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-06-03 18:26:15 +0200

    add regexp to check functions given in input

--- Visualizer/App/forms.py
+++ Visualizer/App/forms.py
@@ -1,9 +1,35 @@
+import re
 from django import forms
+from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
 
 class UploadDatasetForm(forms.Form):
     file = forms.FileField(label='')
     algorithm = forms.ChoiceField([('STSC', 'STSC'), ('OPTICS', 'OPTICS')], widget=forms.RadioSelect(), label='')
 
 class ParametersSTSC(forms.Form):
-    numClusters = forms.DecimalField(required=True,min_value=1,label='# Clusters')
-    k = forms.DecimalField(required=True,min_value=0)
+    numClusters = forms.IntegerField(required=True,min_value=1,label='# Clusters')
+    k = forms.IntegerField(required=True,min_value=0)
+    noiseFunctions = forms.CharField(required=False)
+    sigma = forms.FloatField(required=False)
+    numPoints = forms.IntegerField(required=False, min_value=0)
+
+    def clean(self):
+        cleaned_data = super(ParametersSTSC, self).clean()
+        fun = cleaned_data.get("noiseFunctions")
+        sig = cleaned_data.get("sigma")
+        numPoints = cleaned_data.get("numPoints")
+
+        if (fun != '') and (sig == None):
+            self.add_error("sigma", "Sigma not specified")
+
+        if (fun != '') and (numPoints == None):
+            self.add_error("numPoints", "Number of noise point not specified")
+
+        # Validate function field
+        fun = re.sub('[\s+]', '', fun) #Remove spaces
+        # DON'T FUCK UP WITH THIS LINE.
+        reg = r"(\([x0-9\+\-\*\/\.]+,[0-9]+(\.[0-9]+)?,[0-9]+(\.[0-9]+)?\))(\,(\([x0-9\+\-\*\/\.\ ]+,[0-9]+(\.[0-9]+)?,[0-9]+(\.[0-9]+)?\)))*"
+        if (fun != '') and (not re.fullmatch(reg, fun)) :
+            self.add_error("noiseFunctions", "Format not correct")
+
