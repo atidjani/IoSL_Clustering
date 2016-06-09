6f27caa83bec929359ceb35752ba59b687080f3d
Paul Johannes Everhardus Velthuis <paul.velthuis@campus.tu-berlin.de>
2016-06-09 17:46:24 +0200

    executable R file

--- OPTICS/R/optics_gradient_commandline.R
+++ OPTICS/R/optics_gradient_commandline.R
@@ -1,23 +1,24 @@
 #!/usr/bin/env Rscript
+#the RCCP here the path to the Cpp folder
+Rcpp::sourceCpp('C:/Users/paulv/IoSL_Clustering/OPTICS/R/Gradient_Clustering/gradient_clustering.cpp')
 args = commandArgs(trailingOnly=TRUE)
 library("dbscan")
 
 if (length(args)!=4) {
   stop("missing arguments", call.=FALSE)
 }
-
-dataset <- read.table(args[1],
+#read the dataset
+dataset <- read.table(file =args[1],
                       header = FALSE, sep=",")
-
+#parameters
 epsilon = args[2]
-
 minPoints = args[3]
 t = args[4]
-#converting the dataframe to matrix
-x <- read.table(dataset)
+
+
 
 ## OPTICS
 #gives the optics result
-opt <- optics(x, eps = epsilon, minPts = minPoints)
+opt <- optics(dataset, eps = epsilon, minPts = minPoints)
 #gives the gradient result
 result<-gradient_clustering(opt$order,opt$reachdist,opt$coredist,opt$minPts,t)
