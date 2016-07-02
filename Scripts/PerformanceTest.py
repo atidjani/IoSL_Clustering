63f703671cb35e3076479c7ea67be896848de97c
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-07-02 15:03:07 +0200

    add debug printing

--- Scripts/PerformanceTest.py
+++ Scripts/PerformanceTest.py
@@ -15,7 +15,7 @@ stop = 0.001
 
 
 for size in sizes :
-
+    print size
     X, y = make_blobs(n_samples=size, centers=[[0,-0.5],[4,1],[2,2]], n_features=2,  random_state=0, cluster_std=0.45, center_box=(-10,10))
     f = open(filePath, 'wb')
     for a in X:
@@ -25,6 +25,7 @@ for size in sizes :
     args = [exePath, filePath, str(maxClusters), str(K), str(simCut), str(stop)]
 
     for i in range(0, 3):
+       print i
        start = time.clock()
        proc = s.Popen(args, stdout = s.PIPE)
        proc.wait()
