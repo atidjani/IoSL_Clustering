37cc28b3ecdfcb1372afa6364c728dedf70c31c1
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-07-02 16:03:52 +0200

    add Cpp performance test

---
+++ Scripts/CppPerformanceTest.py
@@ -0,0 +1,40 @@
+import subprocess as s
+import sys, time, os
+
+from sklearn.datasets.samples_generator import make_blobs
+
+sizes = [250, 500, 1000, 1500, 2000, 3000]
+exeTimes = []
+
+exePath = "../STSC/cpp/runner/build/runner"
+filePath = "ds.txt"
+K = 6
+maxClusters = 10
+simCut = 1
+stop = 0.001
+
+
+for size in sizes :
+    print size
+    X, y = make_blobs(n_samples=size, centers=[[0,-0.5],[4,1],[2,2]], n_features=2,  random_state=0, cluster_std=0.45, center_box=(-10,10))
+    f = open(filePath, 'wb')
+    for a in X:
+        f.write((str(a[0]) +','+ str(a[1]) + '\n').encode('utf-8'))
+
+    sizeTime = []
+    args = ['Rscript', exePath, filePath, str(eps), str(min_pts), str(angle)]
+
+    for i in range(0, 3):
+       print i
+       start = time.clock()
+       proc = s.Popen(args, stdout = s.PIPE)
+       proc.wait()
+       stop = time.clock()
+       sizeTime.append(stop-start)
+
+    exeTimes.append(sizeTime)
+    os.remove(filePath)
+
+with open('result.txt', 'wb')  as f :
+    for sizeRun in exeTimes :
+        f.write((str(sizeRun[0]) + ',' + str(sizeRun[1]) + ',' + str(sizeRun[2]) + '\n').encode('utf-8'))
