95f050442eecbf145e4c9035f52c411606ba9bd4
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-07-12 19:09:21 +0200

    update script with new epsilons

--- Scripts/Plotters/Eps.py
+++ Scripts/Plotters/Eps.py
@@ -1,8 +1,8 @@
 import sys,math
 import matplotlib.pyplot as plt
 
-sizes = [250, 500, 1000, 1500, 2000, 2500, 3000, 5000, 10000, 20000]
-epsilons = [0.5, 1, 5]
+sizes = [250, 500, 1000, 1500, 2000, 2500, 3000, 5000, 7500, 10000]
+epsilons = [1, 5, 10]
 
 def mean(vect):
     sum = 0
@@ -27,23 +27,23 @@ with open(sys.argv[1], 'r')  as f:
         print(times)
         resultTimes.append(times)
 
-pfive = []
 one = []
 five = []
+ten = []
 
 for i in range(0, len(sizes)):
-    pfive.append([mean(resultTimes[i*3]), variance(resultTimes[i*3])])
-    one.append([mean(resultTimes[i*3 + 1]), variance(resultTimes[i*3 + 1])])
-    five.append([mean(resultTimes[i*3 + 2]), variance(resultTimes[i*3 + 2])])
+    one.append([mean(resultTimes[i*3]), variance(resultTimes[i*3])])
+    five.append([mean(resultTimes[i*3 + 1]), variance(resultTimes[i*3 + 1])])
+    ten.append([mean(resultTimes[i*3 + 2]), variance(resultTimes[i*3 + 2])])
 
-print(pfive)
 print(one)
 print(five)
+print(ten)
 
 fig = plt.figure(figsize=(9,9))
-plt.errorbar(sizes, [i[0] for i in pfive], yerr=[i[1] for i in pfive], fmt='bo', label="0.5")
 plt.errorbar(sizes, [i[0] for i in one], yerr=[i[1] for i in one], fmt='r^', label="1")
 plt.errorbar(sizes, [i[0] for i in five], yerr=[i[1] for i in five], fmt='y^', label="5")
+plt.errorbar(sizes, [i[0] for i in ten], yerr=[i[1] for i in ten], fmt='y^', label="10")
 plt.xlim([0, 20500])
 plt.xlabel('Dataset Sizes')
 plt.ylabel('Execution time (s)')
