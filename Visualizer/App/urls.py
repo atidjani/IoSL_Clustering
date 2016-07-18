8bfd0282915efa87d8ad4e322851e663d40b7805
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-07-18 18:40:46 +0200

    add url comments

--- Visualizer/App/urls.py
+++ Visualizer/App/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+# Match the url with the right view
 urlpatterns = [
         url(r'^resultSTSC', views.ResultViewSTSC, name='ResultViewSTSC'),
         url(r'^resultOPTICSR', views.ResultViewOPTICSR, name='ResultViewOPTICSR'),
