a7f2fbbb9913061642114a07f94c2ea2124f8c71
Fabio Buso <fabio.buso@campus.tu-berlin.de>
2016-07-18 18:37:13 +0200

    add comments to models

--- Visualizer/App/models.py
+++ Visualizer/App/models.py
@@ -8,13 +8,14 @@ from django.utils.translation import ugettext as _
 from django.core.exceptions import ValidationError
 
 
-# Create your models here.
-
+# Define the model used to store the dataset in the databse
 class Dataset(models.Model):
     creationTime = models.DateTimeField(auto_now_add=True)
     data = models.TextField()
     noise = models.TextField(blank=True)
 
+    # Performs a validation of the dataset uplodaed by the user to
+    # check if is in the required format.
     def clean(self) :
         buffer = io.StringIO(self.data.decode("utf-8"))
         for line in buffer.readlines():
@@ -32,6 +33,7 @@ class Dataset(models.Model):
                    raise ValidationError(_('Format not compatible'))
         return True
 
+    # Returns all the points in the dataset
     def getPoints(self) :
         res = self.data
         if self.noise != '' :
@@ -40,6 +42,8 @@ class Dataset(models.Model):
             res += self.noise
         return res
 
+    # Write the points in the dataset to a file and returns the name of the file.
+    # The name of the file is the md5 of the time.
     def writeFile(self) :
         filePath = "/tmp/" + hashlib.md5(str(time.time()).encode('utf-8')).hexdigest()
         with open(filePath, "w") as f:
